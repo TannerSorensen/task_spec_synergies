d415fbee32c791aeaccd4a095e416f60ca62cf20
Tanner Sorensen <tsorense@usc.edu>
2018-12-14 14:37:28 -0800

    omitting velum from paper

--- paper/graphics/graphs_factors.m
+++ paper/graphics/graphs_factors.m
@@ -1,7 +1,7 @@
 addpath(genpath(fullfile('..','..','analysis','span_contour_processing','functions')))
 addpath(fullfile('..','..','analysis','scripts'))
 
-q = struct('jaw',1,'tng',4,'lip',2,'vel',1,'lar',2);
+q = struct('jaw',1,'tng',4,'lip',3,'vel',1,'lar',2);
 f = 0.2;
 load(fullfile('..','..','analysis','mat','f1_rep1',sprintf('contour_data_jaw%d_tng%d_lip%d_vel%d_lar%d_f%d.mat',q.jaw,q.tng,q.lip,q.vel,q.lar,round(100*f))),'contour_data')
 
